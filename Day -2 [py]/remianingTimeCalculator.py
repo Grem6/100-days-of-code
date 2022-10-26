7844152ada2c68e2b82bdf3f0db12e3f009c3816
Grem <97451337+Grem6@users.noreply.github.com>
2022-10-26 10:00:48 +0530

    Death Calculator added

---
+++ Day -2 [py]/remianingTimeCalculator.py
@@ -0,0 +1,28 @@
+
+print
+
+("""
+      
+      
+      
+______               _    _       _____         _               _         _                
+|  _  \             | |  | |     /  __ \       | |             | |       | |               
+| | | |  ___   __ _ | |_ | |__   | /  \/  __ _ | |  ___  _   _ | |  __ _ | |_   ___   _ __ 
+| | | | / _ \ / _` || __|| '_ \  | |     / _` || | / __|| | | || | / _` || __| / _ \ | '__|
+| |/ / |  __/| (_| || |_ | | | | | \__/\| (_| || || (__ | |_| || || (_| || |_ | (_) || |   
+|___/   \___| \__,_| \__||_| |_|  \____/ \__,_||_| \___| \__,_||_| \__,_| \__| \___/ |_|   
+                                                                                           
+                                                                                           
+                                                                                           
+""")
+print("\n")
+inputAge = int(input('What is your current age?\n'))
+
+years_remaining = 90-inputAge
+daysRemaining = years_remaining*365
+weeksRemaining = years_remaining*52
+monthsRemaining = years_remaining*12
+
+reamainingTime = f"You have {monthsRemaining} months, {weeksRemaining} weeks and {daysRemaining} days remaining"
+
+print(reamainingTime)
